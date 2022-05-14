2fc2b0d91f386d0d1b2c3e0bcaf17254df2c6a2b
UchihaAdil <90258859+UchihaAdil@users.noreply.github.com>
2022-05-14 21:27:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.customer__reviews').slick({
+		infinite: true,
+		slidesToShow: 1,
+    arrows: false,
+		slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    cssEase: 'ease',
+		dots: true
+	});
+})
